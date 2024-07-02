32af20ea7c341bb9d5a2d84966766a1195dda937
beefbarn <70672339+beefbarn@users.noreply.github.com>
2024-07-01 22:06:24 -0400

    button deletes itself

--- gamepage-script.js
+++ gamepage-script.js
@@ -14,6 +14,19 @@ const textTypeEffect = (element, text, i = 0) => {
     setTimeout(() => textTypeEffect(element, text, i + 1), 50);
 }
 
+//faster text typwriter effect
+const fastTypeEffect = (element, text, i = 0) => {
+    element.textContent += text[i];
+
+    if (i === text.length-1) {
+        return;
+    }
+
+    setTimeout(() => fastTypeEffect(element, text, i + 1), 40);
+}
+
+
+
 const scenicText = document.querySelector("#scenic-text");
 const gameText = document.querySelector("#game-text");
 let scenicInitShutdown = 1;
@@ -23,7 +36,7 @@ let gameInitShutdown = 1;
 const scenicInitiation = () => {
     if (scenicInitShutdown === 1) {
         const scenicWriter = document.querySelector("#scenic-writer");
-        textTypeEffect(scenicWriter, `With the door to your cell open, you see a path to freedom...`);
+        fastTypeEffect(scenicWriter, `With the door to your cell open, you see a path to freedom...`);
         scenicInitShutdown = 0
     } else {
         return;
@@ -34,7 +47,7 @@ const scenicInitiation = () => {
 const gameTextInitiation = () => {
     if (gameInitShutdown === 1) {
         const gameWriter = document.querySelector("#game-writer");
-        setTimeout(() => {textTypeEffect(gameWriter, `You wonder what horrors may lurk these prison halls, but you must take your chance...`)}, 4500)
+        setTimeout(() => {fastTypeEffect(gameWriter, `You wonder what horrors may lurk these prison halls, but you must take your chance...`)}, 3400)
         gameInitShutdown = 0
     } else {
         return;
@@ -48,14 +61,17 @@ const spawnNewButton = () => {
     buttonCreation.value = "Next";
     buttonCreation.id = "progressor";
     buttonCreation.addEventListener('click', nextClick);
-    setTimeout(() => {document.getElementById("new-button").appendChild(buttonCreation);}, 10500)
+    setTimeout(() => {document.getElementById("new-button").appendChild(buttonCreation);}, 7700)
 }
 
-//make a function for the "next" button to clear both spans and then count up a progressor of some kind to proceed with the dialogue.
+//make a function for the "next" button to clear both spans and then count up a progressor of some kind to proceed with the dialogue. then removes the "next" button.
 const nextClick = () => {
     const scenicWriter = document.getElementById("scenic-writer");
     const gameWriter = document.getElementById("game-writer");
     gameWriter.textContent = "";
     scenicWriter.textContent = "";
     progressor += 1;
+    const buttonCreation = document.getElementById("progressor");
+    buttonCreation.remove();
 }
+
